198b86d5b66dfb300c657b7e0d3971239621e1e3
Aithusa1 <77977074+Aithusa1@users.noreply.github.com>
2024-02-29 14:32:37 +0100

    Enemy base
    enemies can now follow the player.

--- Examen/Assets/Scripts/Enemy/Enemy.cs
+++ Examen/Assets/Scripts/Enemy/Enemy.cs
@@ -1,27 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public int health = 100;
     public int damage;
+    public float shootingRange = 10f;
+    public float stoppingDistance = 5f;
+
+    private Transform player;
+    private NavMeshAgent navMeshAgent;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= shootingRange)
+        {
+            Attack();
+        }
     }
 
     public virtual void Attack()
     {
-
+        // Implement shooting logic here
+        // For example, instantiate bullets, play shooting animation, etc.
     }
 
     public virtual void TakeDamage(int damage)
@@ -40,10 +55,22 @@ public class Enemy : MonoBehaviour
         Destroy(gameObject);
     }
 
-
     public void Move()
     {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
+        if (distanceToPlayer > stoppingDistance)
+        {
+            // Move towards the player while maintaining a certain distance
+            Vector3 directionToPlayer = player.position - transform.position;
+            Vector3 newPosition = player.position - directionToPlayer.normalized * stoppingDistance;
+            navMeshAgent.SetDestination(newPosition);
+        }
+        else
+        {
+            // Stop moving if within the stopping distance
+            navMeshAgent.ResetPath();
+        }
     }
 
 }
