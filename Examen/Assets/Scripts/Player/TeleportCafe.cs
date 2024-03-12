c399563a107065770b2f46fa3273fd5c0b1a0a76
Aithusa1 <77977074+Aithusa1@users.noreply.github.com>
2024-03-12 13:22:03 +0100

    Cafe Scene in Final Scene

---
+++ Examen/Assets/Scripts/Player/TeleportCafe.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportCafe : MonoBehaviour
+{
+    public Transform pos1;
+    public Transform pos2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
