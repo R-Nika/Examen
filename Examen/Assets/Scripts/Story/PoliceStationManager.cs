f954052ab2dfc568f509a58f07918fcd27fedb3a
Nika <78022387+R-Nika@users.noreply.github.com>
2024-03-13 11:27:15 +0100

    Police Station Functionality
    Changed police station manager to disable phone ringing and load scene when you walk to the door

--- Examen/Assets/Scripts/Story/PoliceStationManager.cs
+++ Examen/Assets/Scripts/Story/PoliceStationManager.cs
@@ -7,17 +7,30 @@ public class PoliceStationManager : MonoBehaviour
 {
     [SerializeField] DialogueSystem dialogueSystem;
     [SerializeField] Animator fadeAnimator;
+    [SerializeField] GameObject missionBoardText;
+    [SerializeField] BoxCollider exitTrigger;
+    [SerializeField] AudioSource phone;
     bool check = true;
     private void Update()
     {
-        if (dialogueSystem.dialogueFinished && check) {
+        if (dialogueSystem.isDialoguing) {
+            phone.Stop();
+        }
 
-            fadeAnimator.SetTrigger("Fade");
-            Invoke("LoadScene", 1.5f);
+        if (dialogueSystem.dialogueFinished && check) {
+            missionBoardText.SetActive(true);
+            exitTrigger.enabled = true;
             check = false;
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && dialogueSystem.dialogueFinished) {
+            fadeAnimator.SetTrigger("Fade");
+            Invoke("LoadScene", 1.5f);
+        }
+    }
     void LoadScene()
     {
         SceneManager.LoadScene(1);
