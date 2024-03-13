4d4987c36767826617cf85d1b68c1702b6aed87f
Nika <78022387+R-Nika@users.noreply.github.com>
2024-03-13 13:59:20 +0100

    Scene Changes
    Modified main menu to have music, changed police scene to have tutorial, changed main city to be darker and added music to the cafe

--- Examen/Assets/Scripts/Player/TeleportCafe.cs
+++ Examen/Assets/Scripts/Player/TeleportCafe.cs
@@ -6,13 +6,14 @@ public class TeleportCafe : MonoBehaviour
 {
     [SerializeField] Transform teleportPos;
     [SerializeField] GameObject rainObj;
-    [SerializeField] AudioSource cafeAudio;
+    [SerializeField] AudioSource cafeAudio, cafeMusic;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) {
             other.transform.position = teleportPos.position;
             rainObj.SetActive(!rainObj.activeInHierarchy);
             cafeAudio.enabled = !cafeAudio.isActiveAndEnabled;
+            cafeMusic.enabled = !cafeMusic.isActiveAndEnabled;
         }
     }
 
